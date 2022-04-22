19e2b44aab5a586c65e66ca56c536f8e87e89e42
Arhum2 <arhumshahzad2003@gmail.com>
2022-04-22 03:33:25 -0400

    extracted and concatenated product and manufacture name from the manifests csv file.

--- manifest.py
+++ manifest.py
@@ -1,10 +1,12 @@
 import csv
-import  sys
+import sys
+from numpy import product
 import pyperclip
-import pandas
+# m16217884
+
 
 # check for command line arguments
-#m16217884
+
 if len(sys.argv) > 1:
     file_name = ''.join(sys.argv[1:]) + ".csv"
     #print(file_name)
@@ -13,26 +15,16 @@ else:
     file_name = ''.join(pyperclip.paste()) + ".csv"
     #print(file_name)
 
+file_path = f"D:\\download\\{file_name}"
 
-# remove un wanted data
+# Read the csv
 
-data = pandas.read_csv(f"D:\download\{file_name}")
-data.drop('Product', inplace=True, axis=1)
-data.drop('Quantity', inplace=True, axis=1)
-data.drop('Manufacturer', inplace=True, axis=1)
-data.drop('UPC', inplace=True, axis=1)
-data.drop('Category', inplace=True, axis=1)
-data.drop('Condition', inplace=True, axis=1)
-data.drop('Retail Price', inplace=True, axis=1)
-data.drop('Total Retail Price', inplace=True, axis=1)
-
-
-# organize .csv data into python list
-
-with open(f"D:\download\{file_name}", "r") as manifest:
-    manifestReader = csv.reader(manifest)  
+with open(f"{file_path}", 'r') as manifest:
+    csv_dict_reader = csv.DictReader(manifest)
     manifestList = []
-    for row in manifestReader:
-        if len (row) != 0:
-            manifestList = manifestList + [row]
-            print(manifestList)
+    for row in csv_dict_reader:
+        item = row['Product'] + ' ' + row['Manufacturer']
+        print(item)
+        #print(row['Product'])
+        #print(row['Manufacturer'])
+
