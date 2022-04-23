689cf3ad6280c50dcb037fcdca81dcac7f980a2c
Arhum2 <arhumshahzad2003@gmail.com>
2022-04-23 03:28:21 -0400

    fixed list bug

--- manifest.py
+++ manifest.py
@@ -1,9 +1,11 @@
 import csv
 import sys
-from numpy import product
 import pyperclip
+from selenium import webdriver
 # m16217884
-
+PATH = 'C:\Program Files (x86)\chromedriver.exe'
+browser = webdriver.Chrome(PATH)
+search = '/html/body/div[1]/div[3]/form/div[1]/div[1]/div[1]/div/div[2]/input'
 
 # check for command line arguments
 
@@ -15,16 +17,20 @@ else:
     file_name = ''.join(pyperclip.paste()) + ".csv"
     #print(file_name)
 
-file_path = f"D:\\download\\{file_name}"
+# assigning .csv file path
+
+file = f"D:\\download\\{file_name}"
 
 # Read the csv
 
-with open(f"{file_path}", 'r') as manifest:
-    csv_dict_reader = csv.DictReader(manifest)
-    manifestList = []
+with open(f"{file}", 'r') as product_list:
+    csv_dict_reader = csv.DictReader(product_list)
+    items = []
     for row in csv_dict_reader:
-        item = row['Product'] + ' ' + row['Manufacturer']
-        print(item)
-        #print(row['Product'])
-        #print(row['Manufacturer'])
+        items.append(row['Product'] + ' ' + row['Manufacturer'])
 
+
+        
+#for row in csv_dict_reader:
+#    browser.get('https://www.google.com/search?q=' + "test")
+    
