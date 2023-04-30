b004b872d71c06d3e66239c519200b86e1c3876b
FIRST_NAME LAST_NAME <arhumshahzad2003@gmail.comclear>
2023-04-30 01:47:41 -0400

    figuring out options

--- automate.py
+++ automate.py
@@ -1,14 +1,21 @@
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.common.by import By
 
-options = Options()
-options.page_load_strategy = 'normal'
+
 #windows
 #And make sure we can find the profile we have just created.
 
 #mac
 #/Users/arhumshahzad/Library/Application Support/Google/Chrome/Default
+options = Options()
+options.page_load_strategy = 'normal'
+
 options.add_argument("user-data-dir=/Users/arhumshahzad/Library/Application Support/Google/Chrome/User Data")
 
 driver = webdriver.Chrome(options=options)
+driver = webdriver.Chrome()
+driver.implicitly_wait(1000)
 driver.get('https://www.youtube.com')
+driver.find_element(By.ID, "input").send_keys('whats good')
+
