4d99bd94730f46ae3d83a55e4581cac5a43e85b5
Arhum2 <arhumshahzad2003@gmail.com>
2023-05-01 04:09:12 -0400

    clicked add photos button

--- automate.py
+++ automate.py
@@ -9,7 +9,7 @@ chrome_service = Service(executable_path="C:\\Program Files (x86)\\chromedriver.
 
 chrome_options = Options()
 chrome_options.add_argument('--user-data-dir=C:\\Users\\pokem\\AppData\\Local\\Google\\Chrome\\User Data')
-chrome_options.add_argument('--profile-directory=Profile 3')
+chrome_options.add_argument('--profile-directory=Default')
 chrome_options.add_argument('--disable-dev-shm-usage')
 chrome_options.add_argument('--no-sandbox')
 chrome_options.add_argument('--remote-debugging-port=9222')
@@ -19,7 +19,10 @@ chrome_options.add_argument('--disable-infobars')
 
 
 browser = webdriver.Chrome(service=chrome_service, options=chrome_options)
-browser.get('https://www.facebook.com')
-
+browser.get('https://www.facebook.com/marketplace/create/item')
 sleep(5)
+browser.find_element(By.XPATH, ("//span[text()='Add Photos']")).click()
+sleep(5)
+
+sleep(10000)
 
