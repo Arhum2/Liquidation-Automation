64dfada4f7d9b66fa2ddf8fe8093e594960036e0
Arhum2 <arhumshahzad2003@gmail.com>
2023-05-07 00:14:14 -0400

    extracting key information from wayfair listing

--- extract.py
+++ extract.py
@@ -27,12 +27,37 @@ class Extract:
 
     def __init__(self, link) -> None:
         self.link = link
+        self.title = 'h1'
+        self.description_button = '//*[@id="CollapseToggle-2"]'
+        self.product_overview = '//*[@id="CollapsePanel-2"]/div/div/div'
+        self.description = None
+        self.features = '//*[@id="CollapsePanel-2"]/div/div/div/div[2]/div/ul'
+        self.dimensions_button = '//*[@id="CollapseToggle-3"]'
+        self.dimensions = '//*[@id="Pres_list_keyval::0"]/h4'
 
         self.browser = uc.Chrome(options=chrome_options, version_main=112)
 
     def extract_info(self):
         self.browser.get(self.link)
-        sleep(100)
+        self.title = self.browser.find_element(By.TAG_NAME, self.title)
+        self.title = self.title._parent.title
+
+        self.description_button = self.browser.find_element(By.XPATH, self.description_button)
+        self.description_button.click()
+        
+        self.product_overview = self.browser.find_element(By.XPATH, self.product_overview)
+        self.description = self.product_overview.text
+
+        self.features = self.browser.find_element(By.XPATH, self.features)
+        self.features = self.features.text
+
+        self.dimensions_button = self.browser.find_element(By.XPATH, self.dimensions_button)
+        self.dimensions_button.click()
+
+        self.dimensions = self.browser.find_element(By.XPATH, self)
+        pass
+        
+        sleep(10000)
 
 with open('G:\\My Drive\\selling\\test\\master_doc.txt', 'r') as links:
     link_list = links.readlines()
